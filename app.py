047fb26a491533cfec63c3338cd8925e9a8cca5d
Konstantin Popov <you@example.com>
2017-12-16 20:59:05 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 import postgresql
 
@@ -13,4 +14,5 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
